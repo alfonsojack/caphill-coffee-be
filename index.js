bf3fe7a042cf5a99927074075a981a29169639b4
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-05 12:24:06 -0700

    removed cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const corsOptions = {
   optionsSucessStatus: 200,
   methods: "GET, POST, OPTIONS",
 };
-app.use(cors());
+// app.use(cors());
 //when you make a request, it will allow them.
 //I will allow get and post methods from localhost 3000.
 //preflight request: request that gets made to the server, but will use the options method. Will allow the response to return a 200.
@@ -26,9 +26,9 @@ app.use(cors());
 app.use(express.json());
 console.log(config);
 
-app.options("/*", (_, res) => {
-  res.sendStatus(200);
-});
+// app.options("/*", (_, res) => {
+//   res.sendStatus(200);
+// });
 //add this, wildcard, ALWAYS return 200 when you make an options request
 
 app.get("/", (request, response) => {
