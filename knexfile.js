e1126a85d386971d24dfb3cf95bc2079d115f197
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-05 17:00:25 -0700

    Clean up BE after deploy

--- knexfile.js
+++ knexfile.js
@@ -17,11 +17,6 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    // user: 'default',
-    // password: 'Pj2izrh8VnoB',
-    // host: 'ep-crimson-silence-53928070.us-east-1.postgres.vercel-storage.com',
-    // port: 5432,
-    // database: 'verceldb',
     connection: process.env.CUSTOM_DATABASE_URL,
     pool: {
       min: 2,
@@ -32,4 +27,3 @@ module.exports = {
     }
   }
 };
-// postgres://default:Pj2izrh8VnoB@ep-crimson-silence-53928070.us-east-1.postgres.vercel-storage.com:5432/verceldb
